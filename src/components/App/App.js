068ac6214548adf09ab0bdebcbd78c723a8f68ff
rafaelmedrib <rafaelmedrib@gmail.com>
2021-09-23 13:56:22 -0300

    Adds a login component conditionally rendered by a token state hook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
+import Login from '../Login/Login';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+
 
 function App() {
-  return <></>
+  
+  const [token, setToken] = useState();
+
+  if(!token){
+    return <Login setToken={setToken} />
+  }
+
+  return <div className = "wrapper" >
+    <h1> Application </h1> 
+    <BrowserRouter>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard/>
+        </Route>
+        <Route path="/preferences">
+          <Preferences />
+        </Route>
+      </Switch> 
+    </BrowserRouter> 
+    </div>
 }
 
 export default App;
